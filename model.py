ea0374791591e41a1f04fda9f94ff5b2884be8a6
shrikant-DS <shrikantvishwakarma2022@gmail.com>
2024-10-06 09:43:59 +0530

    added 3 models for ML

--- model.py
+++ model.py
@@ -1,1 +1,10 @@
 print('adding all ML related code here')
+
+def model_1 :
+    print('adding code for ML model1')
+
+def model_2 :
+    print('adding code for ML model2')
+
+def model_3 :
+    print('adding code for ML model3')
